4cbd473fc55f1a197c10c6aecc9d1b56dc254163
YUHI <dennys.lemoine.1@gmail.com>
2023-04-13 17:14:55 +0200

    start cards for homepage

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,4 +1,27 @@
 import styled from "styled-components";
 import colors from "../../utils/styles/colors";
 
-const
+const CardWrapper = styled.div`
+  display: flex;
+  width: 340px;
+  height: 340px;
+  border-radius: 10px;
+  background-image: linear-gradient(to bottom, #FD6868 0%, rgba(0, 0, 0, 0.9) 90%);
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const CardTitle=styled.span`
+color: white;
+`
+
+function Card() {
+    return (
+        <CardWrapper>
+            <CardTitle>Titre de la location</CardTitle>
+        </CardWrapper>
+    )
+}
+
+export default Card
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,8 +2,9 @@ import '../../styles/App.css';
 import styled from "styled-components";
 import homeImg from "../../assets/Paysage01.png"
 import fonts from "../../utils/styles/fonts";
+import Card from "../../components/card";
 
-const StyledDiv=styled.div`
+const StyledDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -25,19 +26,41 @@ const StyledImg = styled.img`
   background-color: rgba(0, 0, 0, 0.5);
 `
 
-const StyledH1=styled.h1`
+const StyledH1 = styled.h1`
   color: white;
   font-family: ${fonts.primary.name};
   font-weight: 400;
   font-size: 3rem;
 `
 
+const ContainerCards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  justify-items: center;
+  align-items: center;
+  margin-top: 45px;
+  width: 100%;
+  height: 830px;
+  border-radius: 25px;
+  background-color: #f6f6f6;
+`
+
 function Home() {
-  return (
-    <StyledDiv>
-       <StyledH1>Chez vous, partout et ailleurs</StyledH1>
-    </StyledDiv>
-  );
+    return (
+        <div>
+            <StyledDiv>
+                <StyledH1>Chez vous, partout et ailleurs</StyledH1>
+            </StyledDiv>
+            <ContainerCards>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+            </ContainerCards>
+        </div>
+    )
 }
 
 export default Home;
